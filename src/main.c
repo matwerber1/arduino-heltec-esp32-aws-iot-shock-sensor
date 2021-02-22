62680769bc32c38dcab2abb627639412e43294a6
Mathew Werber <werberm@amazon.com>
2021-02-21 19:43:46 -0800

    add shadow light

--- src/main.c
+++ src/main.c
@@ -1,34 +1,38 @@
-#include <MQTTClient.h>
-#include <MQTT.h>       // Be sure to install the MQTT client from Arduino Library Manager authored by Joel Gaehwiler
-#include "heltec.h"
+#include <MQTTClient.h>       // Be sure to install the MQTT client from Arduino Library Manager authored by Joel Gaehwiler
+#include <MQTT.h>
+#include "heltec.h"           // includes library that makes it easier to use OLED
 #include "WiFi.h"
-#include "images.h"
-#include "secrets.h"
 #include <ArduinoJson.h>
 #include <WiFiClientSecure.h>
 #include "dht11.h"
-
+#include "images.h"           // Default Heltec images displayed on OLED
+#include "secrets.h"          // Contains your wifi and AWS IoT credentials/config
 
 // The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_SHOCK_TOPIC   "esp32/shockAlert"
-#define AWS_IOT_PUBLISH_SHADOW_TOPIC   "$aws/things/esp32/shadow/update"
+#define AWS_IOT_PUBLISH_SHOCK_TOPIC "esp32/shockAlert"
+#define AWS_IOT_PUBLISH_SHADOW_TOPIC "$aws/things/esp32/shadow/update"
+#define AWS_IOT_SHADOW_DELTA_TOPIC "$aws/things/esp32/shadow/update/delta"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/alert"
 
+#define MESSAGE_IN_PIN 5      // LED with 100 ohm resistor that lights up when MQTT message received from AWS
+#define SHOCK_LED_PIN 17      // LED with 100ohm resistor that will light up when shock occurs
+#define SHOCK_SENSOR_PIN 35   // I used the shock sensor from this kit: https://www.amazon.com/kuman-K5-USFor-Raspberry-Projects-Tutorials/dp/B016D5L5KE
+#define LIGHT_LED_PIN 18          // Turn this off and on using device state from shadow; 
+#define DHT11_PIN 14            // Temp/Humidity sensor
+
+void writeLine(String str, bool writeToSerial = true);
+
+dht11 DHT11;
 WiFiClientSecure net = WiFiClientSecure();
 MQTTClient client = MQTTClient(256);
-
-const int publishLedPin = 5;   // LED with 100 ohm resistor that lights up when MQTT message received from AWS
-const int shockLedPin = 17;     // LED with 100ohm resistor that will light up when shock occurs
-const int shockPin = 35;        // I used the shock sensor from this kit: https://www.amazon.com/kuman-K5-USFor-Raspberry-Projects-Tutorials/dp/B016D5L5KE
-#define DHT11PIN 14
-dht11 DHT11;
 
 int shockVal;             // 1 if shock detected, 0 if not
 float humidity;           // read from DHT11
 float tempF;              // read from DHT11
 
-long previousMillis = 0;    // will store last time shadow was updated 
-long shadowInterval = 3000;   // how often (ms) we update shadow
+long previousMillis = 0;        // will store last time shadow was updated 
+long shadowInterval = 3000;     // how often (ms) we update shadow
+bool lightEnabled = false;      // the state.reported.lightEnabled value
 
 void setup()
 {
@@ -61,19 +65,20 @@ void setupDisplay() {
 
 void setupPins() {
   
-  pinMode(shockLedPin, OUTPUT);
-  pinMode(publishLedPin, OUTPUT);
-  pinMode(shockPin,INPUT);
+  pinMode(SHOCK_LED_PIN, OUTPUT);
+  pinMode(MESSAGE_IN_PIN, OUTPUT);
+  pinMode(SHOCK_SENSOR_PIN,INPUT);
+  pinMode(LIGHT_LED_PIN, OUTPUT);
 
 }
 
 void connectAWS()
 {
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   writeLine("Connecting to wifi...");
-  Serial.println("Connecting to Wi-Fi");
 
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
@@ -91,7 +96,6 @@ void connectAWS()
   // Create a message handler
   client.onMessage(messageHandler);
 
-  Serial.print("Connecting to AWS IOT");
   writeLine("Connecting to AWS...");
 
   while (!client.connect(THINGNAME)) {
@@ -106,8 +110,8 @@ void connectAWS()
 
   // Subscribe to a topic
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+  client.subscribe(AWS_IOT_SHADOW_DELTA_TOPIC);
 
-  Serial.println("AWS IoT Connected!");
   writeLine("Connected to AWS!");
   delay(2000);
 }
@@ -128,22 +132,55 @@ void publishShockMessage() {
 void messageHandler(String &topic, String &payload) {
 
   Serial.println("incoming: " + topic + " - " + payload);
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-  const String message = doc["msg"];
-  digitalWrite(publishLedPin,HIGH);
-  writeLine(message);
-  delay(2000);
-  digitalWrite(publishLedPin,LOW);
+  
+  if (topic == "esp32/alert") {
+    handleAlertMessage(payload);
+  }
+  else if (topic == AWS_IOT_SHADOW_DELTA_TOPIC) {
+    handleShadowDelta(payload);
+  }
   
 }
 
-String repeatString(String myStr, int count) {
-  String response = myStr;
-  for (int i = 1; i < count; i++) {
-    response = response + myStr;
+void handleShadowDelta(String &payload) {
+
+  StaticJsonDocument<200> doc; 
+  deserializeJson(doc, payload);
+
+  const bool deltaLightEnabled = doc["state"]["lightEnabled"];
+
+  if (deltaLightEnabled.isNull()) {
+    Serial.println("toggling light...");
+    enableShadowLight(deltaLightEnabled);
   }
-  return response;  
+
+}
+
+void enableShadowLight(bool enabled) {
+
+  if (enabled) {
+    Serial.println("Turning light on.");
+    digitalWrite(LIGHT_LED_PIN,HIGH);
+  }
+  else {
+    Serial.println("Turning light off.");
+    digitalWrite(LIGHT_LED_PIN,LOW);
+  }
+
+  lightEnabled = enabled; 
+
+}
+
+// Just print received message to OLED display: 
+void handleAlertMessage(String &payload) {
+
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const String message = doc["msg"];
+  digitalWrite(MESSAGE_IN_PIN,HIGH);
+  writeLine(message);
+  delay(2000);
+  digitalWrite(MESSAGE_IN_PIN,LOW);
 }
 
 void logo(){
@@ -152,7 +189,11 @@ void logo(){
   Heltec.display -> display();
 }
 
-void writeLine(String str) {
+// Write a line to both serial out and OLED display: 
+void writeLine(String str, bool writeToSerial) {
+  if (writeToSerial) {
+    Serial.println(str);
+  }
   Heltec.display -> clear(); 
   Heltec.display -> drawString(10, 0, str);
   Heltec.display -> display();
@@ -160,33 +201,28 @@ void writeLine(String str) {
 
 void checkForShock() {
   
-  shockVal=digitalRead(shockPin);
+  shockVal=digitalRead(SHOCK_SENSOR_PIN);
   
   if(shockVal==HIGH) {
-    digitalWrite(shockLedPin,HIGH);
-    Serial.println("Shock detected...");
+    digitalWrite(SHOCK_LED_PIN,HIGH);
     writeLine("Disturbance in force!");
     delay(1000);
     publishShockMessage();
-    digitalWrite(shockLedPin,LOW);
+    digitalWrite(SHOCK_LED_PIN,LOW);
   }
   else {
-    digitalWrite(shockLedPin,LOW);
-    writeLine("No motion...");
+    digitalWrite(SHOCK_LED_PIN,LOW);
+    writeLine("No motion...", false);
   }
 
 }
 
 void readTempHumidity() {
 
-  int chk = DHT11.read(DHT11PIN);
-  Serial.print("Humidity (%): ");
+  int chk = DHT11.read(DHT11_PIN);
   humidity = (float)DHT11.humidity;
-  Serial.println(humidity, 2);
-  Serial.print("Temperature (F): ");
   float tempC = (float)DHT11.temperature;
   tempF = tempC * 9/5 + 32;
-  Serial.println(tempF, 2);
 
 }
 
@@ -199,21 +235,13 @@ void updateShadow() {
 
 void publishShadow() {
 
-  /*
-  StaticJsonDocument<200> doc;
-  doc["time"] = millis();
-  doc["humidity"] = humidity;
-  doc["tempFahrenheit"] = tempF;
-  char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer);
-  */
-
   DynamicJsonDocument doc(200);
   JsonObject state = doc.createNestedObject("state");
   JsonObject reported = state.createNestedObject("reported");
   reported["time"] = millis();
   reported["humidity"] = humidity;
   reported["tempFahrenheit"] = tempF;
+  reported["lightEnabled"] = lightEnabled;
   
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer);
